5365a0fa0e06a5fd0fc7ccdb11c8bbf741e5ee7c
davidrusyki <davidrusyck@gmail.com>
2022-10-18 18:50:57 -0300

    aula 9

--- JS/client/app/controller/NegociacaoController.js
+++ JS/client/app/controller/NegociacaoController.js
@@ -4,18 +4,16 @@ class NegociacaoController {
         let query = document.querySelector.bind(document);
 
 
-        this._inputData = documento.querySelector("#data");
-        this._inputQuantidade = documento.querySelector("#quantidade");
-        this._inputValor = documento.querySelector("#valor");
-
-
+        this._inputData = document.querySelector("#data");
+        this._inputQuantidade = document.querySelector("#quantidade");
+        this._inputValor = document.querySelector("#valor");
     }
 
     adiciona(event) {
         event.preventDefault();
         console.log('controller funcando');
 
-        let data = new Date(this._inputData.value);
+        let data = new Date(...this._inputData.value.split('-').map((valor, indice) => {valor - indice % 2}));
 
         let negociacao = new negociacao(data, parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
 
